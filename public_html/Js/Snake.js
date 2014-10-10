33e368195730f93f49e6653f0135325bab68b26c
Collin hardy <Collin.Hardy25@gmail.com>
2014-10-10 11:14:00 -0700

    edits and updates to snake

--- public_html/Js/Snake.js
+++ public_html/Js/Snake.js
@@ -1,16 +1,112 @@
+/*--------------------------------------------------------------------------------------------------------
+ *Variables 
+ *--------------------------------------------------------------------------------------------------------
+ */
+
 var snake;
+var snakeLength;
+var snakeSize;
+var snakeDirection;
+
+var food;
 
 var context;
-var screenheight;
-var screenwidth;
+var screenWidth;
+var screenHeight;
+/*--------------------------------------------------------------------------------------------------------
+ * Executing game code
+ * -------------------------------------------------------------------------------------------------------
+ */
+gameInitialize();
+snakeInitialize();
+foodInitialize();
+setInterval(gameLoop, 1000/30);
 
-function gameinitialize() {
+/*--------------------------------------------------------------------------------------------------------
+ * Game functions
+ * -------------------------------------------------------------------------------------------------------
+ */
+function gameInitialize() {
     var canvas = document.getElementById("game-screen");
+    context = canvas.getContext("2d");
     
+    screenWidth = window.innerWidth;
+    screenHeight = window.innerHeight;
+    
+    canvas.width = screenWidth;
+    canvas.height = screenHeight;
 }
-function gameloop () {
-    
+function gameLoop () {
+    gameDraw();
+    snakeUpdate();
+    snakeDraw();
+    foodDraw();
 }
-function gamedraw () {
+function gameDraw () {
+    context.fillStyle = "rgb(96, 185, 235)";
+    context.fillRect(0, 0, screenWidth, screenHeight);
+}
+
+/*--------------------------------------------------------------------------------------------------------
+ * Snake functions
+ * -------------------------------------------------------------------------------------------------------
+ */
+function snakeInitialize() {
+    snake = [];
+    snakeLength = 5;
+    snakeSize = 20;
+    snakeDirection = "down";
     
+    for(var index = snakeLength - 1; index >= 0; index--) {
+        snake.push( {
+            x:index,
+            y:0
+        });
+    }
+}
+
+function snakeDraw() {
+    for(var index = 0; index < snake.length; index++) {
+    context.fillStyle = "white";
+    context.fillRect( snake[index].x * snakeSize, snake[index].y * snakeSize, snakeSize, snakeSize);
+}
+}
+
+function snakeUpdate() {
+    var  snakeHeadX = snake[0].x;
+    var snakeHeadY = snake[0].y;
+    
+    if(snakeDirection == "down") {
+        snakeHeadY++;
+    }
+    
+    var snakeTail = snake.pop();
+    snakeTail.x = snakeHeadX;
+    snakeTail.y = snakeHeadY;
+    snake.unshift(snakeTail);
+}
+
+/*--------------------------------------------------------------------------------------------------------
+ * Food functions
+ * -------------------------------------------------------------------------------------------------------
+ */
+function foodInitialize() {
+    food = {
+        x: 0,
+        y: 0
+    };
+    setFoodPosition();
+}
+
+function foodDraw() {
+    context.fillStyle = "white";
+    context.fillRect( food.x, food.y, snakeSize, snakeSize);
+}
+
+function setFoodPosition() {
+    var randomX = Math.floor(Math.random() * screenWidth); 
+    var randomY = Math.floor(Math.random() * screenHeight);
+    
+    food.x = randomX;
+    food.y = randomY;
 }
